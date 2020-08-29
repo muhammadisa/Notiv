299a21184a7d4efff201f4edd554b0fdacee2b12
muhammadisa <msawjk@gmail.com>
2020-08-29 21:20:49 +0700

    Change call intent style

--- notivlibrary/src/main/java/com/xoxoer/notivlibrary/NotivFirebaseService.kt
+++ notivlibrary/src/main/java/com/xoxoer/notivlibrary/NotivFirebaseService.kt
@@ -3,7 +3,6 @@ package com.xoxoer.notivlibrary
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
-import android.app.TaskStackBuilder
 import android.content.Context
 import android.content.Intent
 import android.os.Build
@@ -14,6 +13,7 @@ import androidx.core.app.NotificationManagerCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 import kotlin.random.Random
+
 
 abstract class NotivFirebaseService constructor(
     private val defaultActivities: AppCompatActivity,
@@ -39,11 +39,12 @@ abstract class NotivFirebaseService constructor(
             defaultActivities::class.java
         }
         val resultIntent = Intent(this, activity)
-        resultIntent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)
-        val resultPendingIntent: PendingIntent = TaskStackBuilder.create(this).run {
-            addNextIntentWithParentStack(resultIntent)
-            getPendingIntent(randomNotificationId, PendingIntent.FLAG_UPDATE_CURRENT)
-        }
+        val resultPendingIntent = PendingIntent.getActivity(
+            this,
+            randomNotificationId,
+            resultIntent,
+            PendingIntent.FLAG_ONE_SHOT
+        )
 
         val notificationManager: NotificationManager =
             getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
